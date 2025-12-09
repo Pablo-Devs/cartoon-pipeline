c0400b8d407f21db750e71b318e699cf11af85f6
Pablo-Devs <blanksonpaul3@gmail.com>
2025-12-09 14:42:51 +0000

    feat: register story router to trpc app router

--- trpc/routers/_app.ts
+++ trpc/routers/_app.ts
@@ -1,7 +1,8 @@
+import { storyRouter } from "@/features/stories/server/routers";
 import { createTRPCRouter } from "../init";
 
 export const appRouter = createTRPCRouter({
-    
+    story: storyRouter
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
